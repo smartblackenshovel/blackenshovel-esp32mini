622b18763ac775dc44a85eb4cda2320b1277a8d4
Lauro Cesar <lauroandradearaujo@gmail.com>
2025-11-14 00:12:17 +0100

    add const params for PINs

--- src/IMUController.cpp
+++ src/IMUController.cpp
@@ -7,7 +7,7 @@ bool IMUController::begin() {
         delete imuWire;
     }
     imuWire = new TwoWire(0);
-    imuWire->begin(4, 3);
+    imuWire->begin(PIN_SDA, PIN_SCL);
     Serial.println("Initializing IMU...");
     if (!imuSensor.begin_I2C(106, imuWire)) {
         Serial.println("Failed to find LSM6DSOX sensor!");
--- src/IMUController.h
+++ src/IMUController.h
@@ -2,6 +2,9 @@
 #include <Adafruit_LSM6DSOX.h>
 #include "IMU.h"
 
+#define PIN_SDA 4
+#define PIN_SCL 3
+
 class IMUController {
     private:
         TwoWire* imuWire;
