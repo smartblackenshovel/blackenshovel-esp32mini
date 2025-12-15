0eb12799319306eb280954dd44482093353bbf8e
Lauro Cesar <lauroandradearaujo@gmail.com>
2025-12-15 17:45:14 +0100

    add parse spots

--- src/utils/JsonUtils.cpp
+++ src/utils/JsonUtils.cpp
@@ -2,14 +2,26 @@
 
 std::vector<User> parseUsers(JsonDocument& doc) {
     std::vector<User> users;
-    JsonArray arr = doc["data"].as<JsonArray>();
+    JsonArray arr = doc.as<JsonArray>();
     for (JsonObject obj : arr) {
         String name = obj["name"].as<String>();
         String id = obj["id"].as<String>();
         users.push_back(User(name, id));
-    }
+    } 
     return users;
 }
+
+std::vector<Spot> parseSpots(JsonDocument& doc) {
+  std::vector<Spot> spots;
+    JsonArray arr = doc.as<JsonArray>();
+    for (JsonObject obj : arr) {
+        String id = obj["id"].as<String>();
+        double lat = obj["lat"].as<double>();
+        double lon = obj["lon"].as<double>();
+        spots.push_back(Spot(id, lat, lon));
+    } 
+    return spots;
+} 
 
 std::vector<String> extractValues(JsonDocument json, const String& key) {
   std::vector<String> result;
--- src/utils/JsonUtils.h
+++ src/utils/JsonUtils.h
@@ -2,8 +2,10 @@
 #include <ArduinoJson.h>
 #include <vector>
 #include "models/User.h"
+#include "models/Spot.h"
 
 std::vector<User> parseUsers(JsonDocument& doc);
+std::vector<Spot> parseSpots(JsonDocument& doc);
 
 std::vector<String> extractValues(JsonDocument json, const String& key);
 
